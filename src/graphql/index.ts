4bb25a41ec91699433875eca639ec702307c8247
Martin Larsson <demdeems@gmail.com>
2022-02-15 18:58:21 +0100

    Add GraphQL validation

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,6 +3,7 @@ import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import { getSchema } from './schema'
 import { Server } from 'http'
 import { Context } from '../types/serverTypes'
+import { validateJWT } from '../utils/auth'
 
 export const graphql = (context: Context, httpServer: Server) => {
   const schema = getSchema(context)
@@ -10,6 +11,15 @@ export const graphql = (context: Context, httpServer: Server) => {
   return new ApolloServer({
     schema,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    context,
+    context: ({ req }) => {
+      const { isValid, payload } = validateJWT(context, req)
+
+      if (!isValid || payload === null) {
+        throw new Error('Error when validating JWT.')
+      }
+
+      const user = { id: payload.id, email: payload.email }
+      return { ...context, user }
+    },
   })
 }
